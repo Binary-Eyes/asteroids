e5f479fca91055eed2e8bc88c2212ebc352b7b0c
amir_barak <binary.eyes.studio@gmail.com>
2025-10-27 20:14:15 +0200

    added game clock

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     is_running = True
+    deltaTime = 0.0
+    game_clock = pygame.time.Clock()
     pygame.init()        
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
@@ -15,6 +17,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        dt = game_clock.tick(60)/1000.0
 
 if __name__ == "__main__":
     main()
