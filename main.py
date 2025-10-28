217dab07d1e4019afb817593a3ddfc733baca992
amir_barak <binary.eyes.studio@gmail.com>
2025-10-28 09:12:34 +0200

    asteroid field is working

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Asteroids v1.0.0")
@@ -8,15 +10,18 @@ def main():
     deltaTime = 0.0
     game_clock = pygame.time.Clock()    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    updateable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-
+    asteroids = pygame.sprite.Group()
     
-    Player.containers = (updateable, drawable)
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    field = AsteroidField()    
     while True:
         # UPDATE
-        for entry in updateable:
+        for entry in updatable:
             entry.update(deltaTime)
 
         for event in pygame.event.get():
