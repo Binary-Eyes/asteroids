2e92703cd810b8f1a9b4ffbeebca81bca352d376
amir_barak <binary.eyes.studio@gmail.com>
2026-03-25 23:19:30 +0200

    stuff

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from logger import log_event
 from circleshape import CircleShape
 from constants import *
 
@@ -11,3 +13,17 @@ class Asteroid(CircleShape):
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        angle = random.uniform(20.0, 50.0)
+        radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid_1 = Asteroid(self.position.x, self.position.y, radius)
+        asteroid_1.velocity = self.velocity.rotate(angle)*1.2
+
+        asteroid_2 = Asteroid(self.position.x, self.position.y, radius)
+        asteroid_2.velocity = self.velocity.rotate(-angle)*1.2
